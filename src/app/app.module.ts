148b3eea0b2d7783f466e3edd1701057ce84f022
First Last <rlotholanl@ip-172-31-19-246.ec2.internal>
2022-05-10 19:40:32 +0000

    Total components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { ChangePasswordComponent } from './components/auth/change-password/change-password.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductViewComponent } from './components/product/product-view/product-view.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserProfileComponent } from './components/users/user-profile/user-profile.component';
+import { OrdersComponent } from './components/users/orders/orders.component';
+import { OrderCreateComponent } from './components/users/orders/order-create/order-create.component';
+import { OrderViewComponent } from './components/users/orders/order-view/order-view.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent,
+    ChangePasswordComponent,
+    ProductComponent,
+    ProductViewComponent,
+    ProductCreateComponent,
+    UsersComponent,
+    UserProfileComponent,
+    OrdersComponent,
+    OrderCreateComponent,
+    OrderViewComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
